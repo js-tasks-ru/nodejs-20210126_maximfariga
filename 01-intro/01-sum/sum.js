1f4c88669669fdd73acbb0e1e1796e4db86784eb
Maxim Fariga <pots1988bear@gmail.com>
2021-01-27 18:03:31 +0200

    Added code for sum function. Added necessary isNumber function for 1-intro 1-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,19 @@
-function sum(a, b) {
-  /* ваш код */
+const isNumber = (value) => typeof value === 'number';
+
+function sum(...rest) {
+  if (rest.length < 2) {
+    throw new SyntaxError('Недостаточно аргументов')
+  }
+
+  const filteredArg = rest.slice(0, 2)
+
+  filteredArg.forEach(item => {
+    if (!isNumber(item)) {
+      throw new TypeError(`${item} не число`)
+    }
+  })
+  
+  return filteredArg.reduce((sum, item) => sum + item, 0)
 }
 
 module.exports = sum;
