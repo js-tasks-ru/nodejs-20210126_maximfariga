cbc7f37b70e3c6c410b61ef00b9ba8a86f84ffbb
Maxim Fariga <pots1988bear@gmail.com>
2021-02-04 21:52:54 +0200

    Added _transform method for LimitSizeStream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,22 @@
-const stream = require('stream');
+const { Transform } = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
-class LimitSizeStream extends stream.Transform {
+class LimitSizeStream extends Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit || null;
+    this.length = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.length += Buffer.byteLength(chunk);
 
+    if (this.length > this.limit) {
+      return callback(new LimitExceededError())
+    } else {
+      callback(null, chunk)
+    }
   }
 }
 
