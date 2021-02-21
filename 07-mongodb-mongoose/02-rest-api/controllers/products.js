090491da0c533c25d5693e952d0e2b72d74387f1
Maxim Fariga <pots1988bear@gmail.com>
2021-02-21 18:34:18 +0200

    Added code for find products by id but it doesn't work

--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,3 +1,18 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+const Category = require('../models/Category');
+
+module.exports.handleInvalidObjectId = async function handleInvalidObjectId(ctx, next) {
+  if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+    ctx.status = 400;
+    ctx.body = 'Invalid id';
+
+    return;
+  }
+
+  next();
+};
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   ctx.body = {};
 };
@@ -7,6 +22,52 @@ module.exports.productList = async function productList(ctx, next) {
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const product = await Product.findById(ctx.params.id);
+  console.log('TRYING TO FIND');
+
+  if (!product) {
+    console.log('NOT FOUND');
+    ctx.status = 404;
+    ctx.body = 'Not Found';
+  } else {
+    console.log('CLIENT ID: ', ctx.params.id);
+    console.log('PRODUCT: ', product);
+    const formattedObj = {
+      id: product._id,
+      title: product.title,
+      images: product.images,
+      category: product.category,
+      subcategory: product.subcategory,
+      price: product.price,
+      description: product.description,
+    };
+
+    console.log('Formatted product:', formattedObj);
+
+    // ctx.body = {product: formattedObj};
+    ctx.status = 200;
+    ctx.body = {title: 'MAX'};
+  }
 };
 
+let count = 0;
+
+module.exports.productCreate = async function productCreate(ctx, next) {
+  const newCount = count++;
+
+  const category = await Category.create({
+    title: `My category ${newCount}`,
+    subcategories: [{title: `My subcategory ${newCount}`}],
+  });
+
+  const product = await Product.create({
+    title: `Some product title ${newCount}`,
+    description: `Some description for product ${newCount}`,
+    price: 100,
+    category: category.id,
+    subcategory: category.subcategories[0].id,
+  });
+
+  ctx.status = 201;
+  ctx.body = product;
+};
