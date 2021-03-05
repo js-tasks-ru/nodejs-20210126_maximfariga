6e70412a470cae800e9fb0960b173eadcea0775c
Maxim Fariga <pots1988bear@gmail.com>
2021-03-05 14:22:17 +0200

    Added try/catch for local strategy

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -4,18 +4,22 @@ const User = require('../../models/User');
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
     async function(email, password, done) {
-      const user = await User.findOne({email});
+      try {
+        const user = await User.findOne({email});
 
-      if (!user) {
-        done(null, false, 'Нет такого пользователя');
+        if (!user) {
+          done(null, false, 'Нет такого пользователя');
+        }
+
+        const isValid = await user.checkPassword(password);
+
+        if (!isValid) {
+          done(null, false, 'Неверный пароль');
+        }
+
+        done(null, user, 'Пользователь найден');
+      } catch (err) {
+        done(err);
       }
-
-      const isValid = await user.checkPassword(password);
-
-      if (!isValid) {
-        done(null, false, 'Неверный пароль');
-      }
-
-      done(null, user, 'Пользователь найден');
     },
 );
